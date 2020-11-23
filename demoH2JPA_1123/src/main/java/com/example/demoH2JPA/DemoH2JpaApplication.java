5212c121daa2ab19f7622d07198f395e0ab7a5a1
ev15963 <ev15963@hs.ac.kr>
2020-11-23 18:55:39 +0900

    demoH2JPA 실행 18:55

---
+++ demoH2JPA_1123/src/main/java/com/example/demoH2JPA/DemoH2JpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoH2JPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoH2JpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoH2JpaApplication.class, args);
+	}
+
+}
---
+++ demoH2JPA_1123/src/test/java/com/example/demoH2JPA/DemoH2JpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoH2JPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoH2JpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
